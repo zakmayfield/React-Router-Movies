ac48425acd2921031bb90a63a253dd32e8698c0c
Zachary Mayfield <zakmayfield@gmail.com>
2019-12-16 15:12:33 -0500

    Set up the client folder, completed up to Part 1, will need to Complete Part 2 and the rest later on

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
-
+import { Route } from 'react-router-dom';
 import SavedList from './Movies/SavedList';
+import MovieList from './Movies/MovieList'
+import Movie from './Movies/Movie';
 
 const App = () => {
   const [savedList, setSavedList] = useState( [] );
@@ -12,7 +14,14 @@ const App = () => {
   return (
     <div>
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+
+      <Route exact path="/">
+        <MovieList />
+      </Route>
+
+      <Route path="/movie/:id">
+        <Movie />
+      </Route>
     </div>
   );
 };
